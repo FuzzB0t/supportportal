a4ee6664254a00a238b5c468266928ba20b33dbd
mamonticello <micah.monticello@gmail.com>
2018-09-19 08:43:17 -0700

    New Admin picture, fonts

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'users/profile', to: 'pages#user'
+  get 'util/licenses', to: 'license_decoder#index'
+  post 'util/licenses', to: 'license_decoder#upload'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
