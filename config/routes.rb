297cb00a12e36cd1a702a6ca396cd1e9891c7e02
yosupgurl <benabitbul@hotmail.com>
2018-09-22 17:08:15 -0400

    tried creating a functional user create panel but didn't work, pushing changes only because they're listed as staged, will continue with cancan gem

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,5 @@ Rails.application.routes.draw do
   post 'util/licenses', to: 'license_decoder#upload'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+   
